7afa4cc3dd6d21fd049b72069aae64020a472b32
Francisco Costa <francisco.costa@evolutionit.com.br>
2018-09-12 08:53:14 -0300

    add redux

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { getAllEmployees } from '../../services/employeeService';
-// import * as actions from '../../store/actions/index';
+import * as actions from '../../store/actions/index';
 
 import Header from '../../components/Header/Header';
 import SearchBar from '../../components/SearchBar/SearchBar';
@@ -11,71 +10,48 @@ import EmployeeList from '../../components/EmployeeList/EmployeeList';
 import './HomePage.css';
 
 class HomePage extends Component {    
-    constructor(props) {
-        super(props);
-
-        const allEmployees = getAllEmployees();
-
-        this.state = {            
-            employees: allEmployees,
-            filteredEmployees: allEmployees,
-            selectedEmployeeName: ''
-        }    
+    componentDidMount() {
+        this.props.onInitEmployees();        
     }
-    
-    // componentDidMount() {
-    //     this.props.onInitEmployees();        
-    // }
 
     handleSearchTextChange = (event) => {
         const searchTerm = event.target.value;
 
-        let filteredEmployees = 
-            this.state.employees.filter(employee => employee.name.indexOf(searchTerm) !== -1);
-
-        this.setState({
-            filteredEmployees: filteredEmployees
-        });
-
-        // this.props.onSearchEmployees(searchTerm);
+        this.props.onSearchEmployees(searchTerm);
     }
 
     handleEmployeeListItemClick = (event, employeeName) => {
-        this.setState({ selectedEmployeeName: employeeName });
-
-        // this.props.onSelectEmployee(employeeName);
+        this.props.onSelectEmployee(employeeName);
     }
 
     render() {        
         return (
             <div className="container">
                 <Header 
-                    selectedEmployeeName={this.state.selectedEmployeeName} />
+                    selectedEmployeeName={this.props.selectedEmployeeName} />
                 <SearchBar 
                     handleSearchTextChange={this.handleSearchTextChange} />
                 <EmployeeList 
-                    employees={this.state.filteredEmployees}
+                    employees={this.props.filteredEmployees}
                     handleEmployeeListItemClick={this.handleEmployeeListItemClick} />
             </div>
         );
     }
 }
 
-export default HomePage;
+const mapStateToProps = state => {    
+    return {
+        filteredEmployees: state.employeeReducer.filteredEmployees || [],
+        selectedEmployeeName: state.employeeReducer.selectedEmployeeName
+    };
+}
 
-// const mapStateToProps = state => {    
-//     return {
-//         filteredEmployees: state.employeeReducer.filteredEmployees || [],
-//         selectedEmployeeName: state.employeeReducer.selectedEmployeeName
-//     };
-// }
+const mapDispatchToProps = dispatch => {
+    return {
+        onInitEmployees: () => dispatch(actions.initEmployees()),
+        onSearchEmployees: (searchTerm) => dispatch(actions.searchEmployees(searchTerm)),    
+        onSelectEmployee: (employeeName) =>  dispatch(actions.selectEmployee(employeeName)),    
+    }
+}
 
-// const mapDispatchToProps = dispatch => {
-//     return {
-//         onInitEmployees: () => dispatch(actions.initEmployees()),
-//         onSearchEmployees: (searchTerm) => dispatch(actions.searchEmployees(searchTerm)),    
-//         onSelectEmployee: (employeeName) =>  dispatch(actions.selectEmployee(employeeName)),    
-//     }
-// }
-
-// export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
--- src/index.js
+++ src/index.js
@@ -4,26 +4,26 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-// import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 
 import employeeReducer from './store/reducers/employeeReducer';
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const rootReducer = combineReducers({
-//     employeeReducer
-// });
+const rootReducer = combineReducers({
+    employeeReducer
+});
 
-// const store = createStore(rootReducer, composeEnhancers(
-//     applyMiddleware(thunk)
-// ));
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
-const app = (<App />
-    // <Provider store={store}>
-    //     <App />
-    // </Provider>
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
